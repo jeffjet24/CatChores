2923d80a2790c4eda2a4d092031f50be7dc0276b
Mack Chamberlain <Mack.Chamberlain@infor.com>
2016-10-05 16:14:07 -0500

    Big Updates to Script. Hooked up Backend and updating of UI on Event

--- scripts/apiGateway-js-sdk/apigClient.js
+++ scripts/apiGateway-js-sdk/apigClient.js
@@ -51,7 +51,7 @@ apigClientFactory.newClient = function (config) {
         config.defaultAcceptType = 'application/json';
     }
 
-    
+
     // extract endpoint and path from url
     var invokeUrl = 'https://5vsoxh69gj.execute-api.us-east-1.amazonaws.com/v1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
@@ -80,14 +80,14 @@ apigClientFactory.newClient = function (config) {
     };
 
     var apiGatewayClient = apiGateway.core.apiGatewayClientFactory.newClient(simpleHttpClientConfig, sigV4ClientConfig);
-    
-    
-    
+
+
+
     apigClient.catEventPost = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
-        
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var catEventPostRequest = {
             verb: 'post'.toUpperCase(),
             path: pathComponent + uritemplate('/cat-event').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -95,17 +95,17 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(catEventPostRequest, authType, additionalParams, config.apiKey);
     };
-    
-    
+
+
     apigClient.statusUpdateGet = function (params, body, additionalParams) {
         if(additionalParams === undefined) { additionalParams = {}; }
-        
+
         apiGateway.core.utils.assertParametersDefined(params, [], ['body']);
-        
+
         var statusUpdateGetRequest = {
             verb: 'get'.toUpperCase(),
             path: pathComponent + uritemplate('/status-update').expand(apiGateway.core.utils.parseParametersToObject(params, [])),
@@ -113,11 +113,11 @@ apigClientFactory.newClient = function (config) {
             queryParams: apiGateway.core.utils.parseParametersToObject(params, []),
             body: body
         };
-        
-        
+
+
         return apiGatewayClient.makeRequest(statusUpdateGetRequest, authType, additionalParams, config.apiKey);
     };
-    
+
 
     return apigClient;
 };
--- scripts/buttonPress.js
+++ scripts/buttonPress.js
@@ -1,46 +1,51 @@
 function sendEvent(data) {
-  // Data Shape: {name, eventName}
-  var XHR = new XMLHttpRequest();
-  var urlEncodedData = "";
-  var urlEncodedDataPairs = [];
-  var name;
+  //Data Shape: {name, eventName}
   var curTime = new Date().getTime() / 1000;
   var dataMessage = {"event": data.eventName, "name": data.name, "time": curTime};
 
-  // We turn the data object into an array of URL encoded key value pairs.
-  for(name in dataMessage) {
-    urlEncodedDataPairs.push(encodeURIComponent(name) + '=' + encodeURIComponent(dataMessage[name]));
-  }
-
-  // We combine the pairs into a single string and replace all encoded spaces to
-  // the plus character to match the behaviour of the web browser form submit.
-  urlEncodedData = urlEncodedDataPairs.join('&').replace(/%20/g, '+');
-
-  // We define what will happen if the data is successfully sent
-  XHR.addEventListener('load', function(event) {
-    alert('Event Sent Successfully. :) ');
-  });
-
-  // We define what will happen in case of error
-  XHR.addEventListener('error', function(event) {
-    alert('The Request Did not get sent correctly. :( ');
-  });
-
-  // We setup our request
-  XHR.open('POST', 'https://5vsoxh69gj.execute-api.us-east-1.amazonaws.com/v1/cat-event');
-
-  // We add the required HTTP header to handle a form data POST request
-  XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  //XHR.setRequestHeader('Content-Length', urlEncodedData.length);
-  XHR.setRequestHeader('Access-Control-Allow-Origin', '*');
-  // And finally, We send our data.
-  XHR.send(urlEncodedData);
+  var apigClient = apigClientFactory.newClient();
+  var params = {};
+  var body = dataMessage;
+  var additionalParams = {};
+  apigClient.catEventPost(params, body, additionalParams)
+      .then(function(result){
+          //This is where you would put a success callback
+          document.getElementById("successMessage").style.visibility = "visible";
+          updatePerformed(data.eventName, data.name, curTime);
+      }).catch( function(result){
+          //This is where you would put an error callback
+          //alert('The Request Did not get sent correctly. :( :' + result);
+          document.getElementById("failMessage").style.visibility = "visible";
+      });
 }
-
 
 
 function updatePerformed(eventName, userName, timePer){
   // This function is going to update all the panels that are related and the performed section.
+  const BASE_BUTTON_PANEL = "panel margin0";
+  const BASE_BUTTON = "btn btn-sm";
+  const DROPDOWN_BUTTON = "dropdown-toggle";
+  const BASE_PER_PANEL = "panel margin0 performedPanel text-center";
+  var buttonPanel = eventName;
+  var panelName = eventName + "PerPanel";
+  var textName = eventName + "PerText";
+  var time = new Date(timePer * 1000);
 
+  // Setting the Button Panel Color
+  if(buttonPanel != "MillieNails" && buttonPanel != "KittyXNails"){
+    document.getElementById(buttonPanel).className = BASE_BUTTON_PANEL + " panel-success";
+  } else {
+    var classString = BASE_BUTTON + " btn-success";
+    if(buttonPanel == "KittyXNails"){
+      classString = classString + " " + DROPDOWN_BUTTON;
+    }
+    document.getElementById(buttonPanel).className = classString;
+  }
 
+  // Setting the Performed Panel Colors
+  document.getElementById(panelName).className = BASE_PER_PANEL + " panel-success";
+
+  // Setting the Performed Panel Text
+  var dateString = time.getMonth().toString() + "/" + time.getDate().toString() + " " + time.getHours().toString() + ":" + time.getMinutes().toString();
+  document.getElementById(textName).innerHTML = dateString + " By " + userName;
 }
