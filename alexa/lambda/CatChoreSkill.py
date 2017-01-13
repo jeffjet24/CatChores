f1e69bbeaae23676b342bd8a49d749f50149d032
Mack Chamberlain <mack.chamberlain@infor.com>
2017-01-13 13:48:19 -0600

    Adding the ability to query for the last time a chore was done

--- alexa/lambda/CatChoreSkill.py
+++ alexa/lambda/CatChoreSkill.py
@@ -7,6 +7,7 @@ snsClient = boto3.client('sns')
 lambdaClient = boto3.client('lambda')
 catEventSNS = "arn:aws:sns:us-east-1:818316582971:CatEvent"
 catGetPerformedLambda = "arn:aws:lambda:us-east-1:818316582971:function:CatGetPerformed"
+TIMEZONE_DIFFERENCE = timedelta(hours = -6)
 # --------------- Helpers that build all of the responses ----------------------
 
 def build_speechlet_response(title, output, reprompt_text, should_end_session):
@@ -106,6 +107,9 @@ def performChore(intent, session, event):
     utc_dt = datetime.strptime(event["request"]["timestamp"], '%Y-%m-%dT%H:%M:%SZ')
     time = int((utc_dt - datetime(1970, 1, 1)).total_seconds())
 
+    if eventName == "FeedAM" and utc_dt.hour >= 12:
+        eventName = "FeedPM"
+
 
     post_message = {'name': actorName, "time": str(time), "event": eventName}
 
@@ -121,7 +125,6 @@ def performChore(intent, session, event):
 
 def getPerformedChore(intent, session, event):
     card_title = intent['name']
-    should_end_session = True
 
     latestChores = lambdaClient.invoke(
         FunctionName = catGetPerformedLambda,
@@ -130,12 +133,82 @@ def getPerformedChore(intent, session, event):
     print("Response: " + str(latestChores))
     payload = latestChores['Payload'].read()
 
-    choresList = json.loads(json.loads(payload)['body'])['Message']
+    choresList = json.loads(json.loads(json.loads(payload)['body'])['Message'])
     print("Parsed JSON Chore List: " + json.dumps(choresList, indent=2))
 
-    speech_output = ""
-    return build_response({}, build_speechlet_response(
-        card_title, speech_output, "", should_end_session))
+    eventName = ""
+    if "value" in event["request"]["intent"]["slots"]["Cat"]:
+        catName = event["request"]["intent"]["slots"]["Cat"]["value"]
+        eventName = getActivity(event["request"]["intent"]["slots"]["Chore"]["value"], catName)
+    else:
+        eventName = getActivity(event["request"]["intent"]["slots"]["Chore"]["value"], "")
+
+    selecteditem = {}
+    if eventName != "FeedAM" and eventName != "FeedPM":
+        for task in choresList:
+            taskItem = task['item']
+            if taskItem['task'] == eventName:
+                selecteditem = task
+                break
+    else:
+        feedAMTask = {}
+        feedPMTask = {}
+        for task in choresList:
+            #print("Cur Task" + task)
+            taskItem = task['item']
+            if taskItem['task'] == "FeedAM":
+                feedAMTask = task
+            elif taskItem['task'] == "FeedPM":
+                feedPMTask = task
+            if feedAMTask and feedPMTask:
+                break
+
+        if int(feedAMTask['item']['time']) < int(feedPMTask['item']['time']):
+            selecteditem = feedPMTask
+        else:
+            selecteditem = feedAMTask
+
+    if selecteditem:
+        lastTimeStamp = selecteditem['item']['time']
+        lastPerson = selecteditem['item']['personName']
+        lastStatus = selecteditem['status']
+        lastChore = selecteditem['item']['task']
+
+        lastDateTime = datetime.fromtimestamp(int(lastTimeStamp)) + TIMEZONE_DIFFERENCE
+        lastTime = lastDateTime.strftime("%I:%M%p")
+        lastDay = '{0:%A}, {0:%B} {0:%d}'.format(lastDateTime)
+        speech_output = ""
+
+        if lastChore == "FeedAM" or lastChore == "FeedPM":
+            speech_output = speech_output + "The cat was last fed by "
+        elif lastChore == "DownstairLitter":
+            speech_output = speech_output + "The downstairs litter was last emptied by "
+        elif lastChore == "UpstairLitter":
+            speech_output = speech_output + "The upstairs litter was last emptied by "
+        elif lastChore == "Vacuum":
+            speech_output = speech_output + "The carpet was last vacuumed by "
+        elif lastChore == "MillieNails":
+            speech_output = speech_output + "Millie's nails were last clipped by "
+        elif lastChore == "KittyXNails":
+            speech_output = speech_output + "Kitty X's nails were last clipped by "
+
+
+        speech_output = speech_output + str(lastPerson) + " at "+ str(lastTime) + " on " + str(lastDay) + ". "
+
+        if lastStatus == "green":
+            speech_output = speech_output + "The chore has been done recently, and does not need to be done at this time."
+        elif lastStatus == "yellow":
+            speech_output = speech_output + "The chore should be done soon, however can go a little bit longer before needing to be done."
+        else:
+            speech_output = speech_output + "The chore needs to be done. Please do the chore as soon as possible."
+
+        print("Speech Output: " + speech_output)
+        return build_response({}, build_speechlet_response(
+            card_title, speech_output, "", True))
+    else:
+        speech_output = "Please make your request again, I did not understand your query. "
+        return build_response({}, build_speechlet_response(
+            card_title, speech_output, "Please ask me when a chore was done last or tell me that a chore has been completed. ", False))
 
 
 # --------------- Events ------------------
